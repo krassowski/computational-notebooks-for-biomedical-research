4feaf60d4a3359d4b2dfa2cb01c12df3d59c2511
krassowski <krassowski.michal@gmail.com>
2020-07-16 19:17:37 +0100

    Fix wget call after moving from notebook

--- data/download.sh
+++ data/download.sh
@@ -1,2 +1,2 @@
-!wget https://github.com/jupyterlab/jupyterlab-demo/raw/077f5d68c18dadca2cd6431c23267ac5486d8f9e/data/zika_assembled_genomes.fasta
-!wget https://data.kitware.com/api/v1/file/564a5b078d777f7522dbfaa6/download -O 005_32months_T2_RegT1_Reg2Atlas_ManualBrainMask_Stripped.nrrd -nc
+wget https://github.com/jupyterlab/jupyterlab-demo/raw/077f5d68c18dadca2cd6431c23267ac5486d8f9e/data/zika_assembled_genomes.fasta
+wget https://data.kitware.com/api/v1/file/564a5b078d777f7522dbfaa6/download -O 005_32months_T2_RegT1_Reg2Atlas_ManualBrainMask_Stripped.nrrd -nc
